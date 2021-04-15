2a311d4174e976464ef2687333c192f405e2f644
Jason <m.jason.liu@outlook.com>
2021-04-15 16:59:02 +0800

    use custom light

--- packages/loadgltf/src/index.ts
+++ packages/loadgltf/src/index.ts
@@ -4,6 +4,7 @@ import {
   SceneLoader,
   ArcRotateCamera,
   Vector3,
+  HemisphericLight,
 } from '@babylonjs/core'
 import '@babylonjs/loaders/glTF'
 
@@ -19,15 +20,18 @@ const camara = new ArcRotateCamera(
   new Vector3(0, 0, 0),
   scene
 )
+camara.attachControl(true)
+
+const light = new HemisphericLight('light', new Vector3(-50, 0, 0), scene)
 
 SceneLoader.Append('./assets/', 'scene.gltf', scene, (scene) => {
   // Create a default arc rotate camera and light.
-  scene.createDefaultCameraOrLight(true, true, true)
-
+  //   scene.createDefaultCameraOrLight(false, true, true)
   // The default camera looks at the back of the asset.
   // Rotate the camera by 180 degrees to the front of the asset.
-  // @ts-ignore
-  scene.activeCamera.alpha += Math.PI
+  //   const camera: ArcRotateCamera = scene.activeCamera as ArcRotateCamera
+  //   camera.alpha += Math.PI
+  //   camera.position.add(new Vector3(111111, 11, 11))
 })
 // SceneLoader.ImportMesh('', './assets/', 'scene.gltf', scene)
 
